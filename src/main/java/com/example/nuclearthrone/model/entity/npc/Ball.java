a287b0512212429b17f39e704c6d1308b6e8dea2
Andres Parra <andresdavidparra2002@gmail.com>
2023-06-07 22:16:45 -0500

    winner screen

--- src/main/java/com/example/nuclearthrone/model/entity/npc/Ball.java
+++ src/main/java/com/example/nuclearthrone/model/entity/npc/Ball.java
@@ -11,10 +11,12 @@ import com.example.nuclearthrone.model.util.Images;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
 public class Ball extends MovableEntity {
 
+    public static AnchorPane winner;
     public static final double WIDTH = 30;
     public static final double HEIGHT = 30;
     public static final int HEALTH = -1;
@@ -58,7 +60,7 @@ public class Ball extends MovableEntity {
         if(distance < DISTANCE){
             moveAwayFrom(Avatar.getInstance());
             if(intersects(1185,315,30,100) && !Level.enemiesLeft()){
-                System.out.println("you won");
+                winner.setVisible(true);
             }
         }
     }
